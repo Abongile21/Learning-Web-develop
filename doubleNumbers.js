af77d3cc2215731c3567b0a1adcd5ea87f8224e9
Abongile21 <abongiletheo00@gmail.com>
2024-03-20 16:40:25 +0200

    added program used to double each element of an an array

---
+++ doubleNumbers.js
@@ -0,0 +1,35 @@
+const gArr = [26,1,2,3,4,5,6,7,8,9,10,11,12]
+
+
+
+function doubleNumber(Arr,n){
+   let myArr= []
+
+   for (index =0 ; index<Arr.length ;index++){
+      myArr.push(Arr[index],n)
+   }
+   //return myArr
+}
+console.log(doubleNumber(gArr,2))
+
+// }
+// console.log(myArr)
+
+// for (index=0; index<gArr.length; index++){
+    
+//      if (gArr[index]%2==0){
+//         myArr.push(gArr[index])
+
+
+//      }
+    
+
+
+//     console.log(`--------------------Pushing into myArr step ${index+1} --------------------------`)
+
+//     console.log(myArr)
+//     console.log()
+
+// }
+// console.log("------------------ -My Array---------------------------")
+// console.log(myArr)
