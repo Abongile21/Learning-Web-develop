29c4f59ead3140e34bf848c0e93cc6d9b28e2c92
Abongile21 <abongiletheo00@gmail.com>
2024-03-20 16:41:15 +0200

    notes about intro to JS

---
+++ index.js
@@ -0,0 +1,84 @@
+
+
+// JS is a dynamic and weakly typed language
+//
+//-meaning you can reassign a variable with a different data type
+//
+//Data types
+//-------------Primitive data types--------------
+//-Number
+//-Boolean
+//-undefined
+//-Symbol
+    //
+//-Bigint
+
+
+//-String[immutable data type]
+    /*string literal: ""
+    template strings: uses back ticks (interpolation)
+    -`My name is,${firstname}, ${last name}`
+    */
+
+
+
+//------------------Non-primitive data types----------------
+//-Object {}
+    /*let myObject = {}
+    ---------------value assignment in an object----------------
+    {key:value}
+    myObject.name ="Abongile" or,
+    myObject['name'] ="Abongile"
+    -------------------printing out a value -------------------
+     console.log(myObject.name)
+    */
+
+//--------------------------------------------------------------------------  //
+//-Arrays []- mutable data type
+    //multiple values and they are heterogeneous.
+
+
+//-Functions 
+    /* function greet(){
+        console.log("Hello hi!")
+    }*/
+
+    //
+    /*
+    //addition of two inputs
+    function sum(x,y){
+        return x+y
+    }
+    console.log(2,1) >> 3
+    
+    
+    
+    */
+   /**
+    * Arrow function
+    * let add =(x,y)=>{
+        return x+y
+    }
+    * 
+    * 
+    * 
+    */
+
+//Declaring variables in  Js
+var name1= "Dj";
+let name2 ="Junior"
+const name3 = "Yolanda"
+
+name1 = "Buhle"
+
+console.log(name1)
+
+const fruits= ["mango", "banana", "orange", "grapes", "apple"]
+
+const mango = fruits.splice(2)
+//const orange = fruits.pop()
+console.log(mango) 
+
+const adding = 1+"1"
+console,console.log("1"===1);
+
