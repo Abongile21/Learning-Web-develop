5bc238273e70b9e68593cb5bb71f0939407df322
Abongile21 <abongiletheo00@gmail.com>
2024-03-20 16:44:11 +0200

    Added a program used to print a triangle characters

---
+++ triangle.js
@@ -0,0 +1,14 @@
+for (start=6;start>0;start--){
+    let row= ""
+    for (start2 =1;start2<=start;start2++){
+        row += "*"    
+    }
+    console.log(row)
+}
+for (start=1;start<=6;start++){
+    let row= ""
+    for (start2 =1;start2<=start;start2++){
+        row += "*"    
+    }
+    console.log(row)
+}
